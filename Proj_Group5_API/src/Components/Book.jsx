8ef05b49a2a05a377e05e9cc73124dbb85207066
Hinal22i <hinaljagani4@gmail.com>
2024-04-12 14:24:12 +0200

    route change 2

--- Proj_Group5_API/src/Components/Book.jsx
+++ Proj_Group5_API/src/Components/Book.jsx
@@ -1,11 +1,18 @@
 // import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Book = ({ key, title, imageUrl, descriptionLink }) => {
+  const navigate = useNavigate();
+
+    const goSomewhere = () => {
+        navigate(`/bookshelf/${descriptionLink}`)
+      }
+
   return (
     <div className="book">
       <img src={imageUrl} alt={imageUrl} />
       <div className="book-title">{title}</div>
-      <a className="book-link" href={`/BookShelf/${descriptionLink}`}>
+      <a className="book-link" onClick={goSomewhere}>
         Learn More
       </a>
     </div>

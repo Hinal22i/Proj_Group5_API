13179cc3c84e78913f3ec7a6e8ee62cb82a6cd0e
mail@sofiehilt.org <sofie@w60.fritz.box>
2024-04-12 13:22:55 +0200

    deleted components and changed link

--- Proj_Group5_API/src/App.jsx
+++ Proj_Group5_API/src/App.jsx
@@ -3,7 +3,7 @@ import MainContent from "./Components/MainContent";
 // import Category from "./Components/Category";
 // import Footer from "./Components/Footer";
 import BookItem from "./Components/BookItem";
-import BookShelf from "./components/BookShelf";
+import BookShelf from "./Components/BookShelf";
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from "./Layouts/MainLayout";
 import "./App.css";
